f673fb916f61e2d33580dc4e04c42cfd45ee4c90
Anthony Robert <anthonyrobert1997@hotmail.fr>
2018-09-23 11:54:48 -0400

    début histoire

--- Assets/Scripts/OnSceneLoad.cs
+++ Assets/Scripts/OnSceneLoad.cs
@@ -13,17 +13,19 @@ public class OnSceneLoad : MonoBehaviour {
 	    {
             Debug.Log("Chapeau");
 	        Instantiate(Resources.Load("Player"));
-	    }
+	        GameObject.Find("Gun").GetComponent<Weapon>().FireRate = StaticVariables.WeaponDamage;
+        }
 	    else
 	    {
-            Cursor.SetCursor();
+            
 	        Debug.Log("Blondin");
             Instantiate(Resources.Load("blondin"));
         }
 
 	    gameObject.AddComponent<CameraController>().entity = GameObject.FindGameObjectWithTag("Player");
+	    GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(0, -2);
 
-	    if (StaticVariables.CoffreSousSolOuvert)
+        if (StaticVariables.CoffreSousSolOuvert)
 	    {
 	        GameObject obj = GameObject.Find("rockfirstlvl");
 	        if (obj != null)
@@ -32,9 +34,50 @@ public class OnSceneLoad : MonoBehaviour {
 	        }
 	    }
 
-        if (StaticVariables.CowboyChapeau)
-            GameObject.Find("Gun").GetComponent<Weapon>().FireRate = StaticVariables.WeaponDamage;
+	    switch (SceneManager.GetActiveScene().name)
+	    {
+            case "HouseInterior":
+                OnInteriorEnter();
+                break;
+
+            case "HouseUnderground":
+                OnUndergroundEnter();
+                break;
+
+            case "MainMap":
+                OnMainMapEnter();
+                break;
+	    }
+
     }
-	
-    
+
+    void OnInteriorEnter()
+    {
+        if (!StaticVariables.InteriorTextShown)
+        {
+            GameObject.Find("TextInterior").GetComponent<RPGTalk>().NewTalk("", "");
+            StaticVariables.InteriorTextShown = true;
+        }
+    }
+
+    void OnUndergroundEnter()
+    {
+
+    }
+
+    void OnMainMapEnter()
+    {
+        if (!StaticVariables.FirstTextShown)
+        {
+            Debug.Log("spawn -2 -33");
+            GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(-2, -33);
+            RecenterCameraToPlayer();
+        }
+    }
+
+    void RecenterCameraToPlayer()
+    {
+        Vector3 pos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        gameObject.GetComponent<Camera>().transform.position = new Vector3(pos.x, pos.y, -10);
+    }
 }
