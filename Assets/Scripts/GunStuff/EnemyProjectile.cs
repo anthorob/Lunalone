385d4c483b136cb38ce2ff940fef845a9fef2ff8
Antoine Veillette <antoine.veillette@shifttech.ca>
2018-09-23 11:36:34 -0400

    Gun done + spawn

---
+++ Assets/Scripts/GunStuff/EnemyProjectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour {
+
+    public float speed = 10;
+    private GameObject target;
+    private Vector3 targetPos;
+    private Vector2 moveDirection;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        targetPos = target.transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+        if (transform.position == targetPos)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Collision");
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision");
+        }
+    }
+}
