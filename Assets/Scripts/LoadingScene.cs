cd9fc0204398e82680237c195edf8eaf53890417
Anthony Robert <anthonyrobert1997@hotmail.fr>
2018-09-23 08:24:03 -0400

    start interaction mainmap

--- Assets/Scripts/LoadingScene.cs
+++ Assets/Scripts/LoadingScene.cs
@@ -28,7 +28,7 @@ public class LoadingScene : MonoBehaviour {
         // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
         // a sceneBuildIndex of 1 as shown in Build Settings.
 
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("rompre");
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainMap");
 
         // Wait until the asynchronous scene fully loads
         while (!asyncLoad.isDone)
